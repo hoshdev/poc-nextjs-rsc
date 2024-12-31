38efca31b3b4d5b7332437f4c6487d565902cfdf
Jose CM <jose.coego@gmail.com>
2024-12-31 14:25:29 +0100

    feat: Add hello comonent

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Hello from "@/components/Hello";
 import { getPosts } from "@/lib/getPost";
 import { Metadata } from "next";
 
@@ -19,6 +20,7 @@ const PostsPage = async () => {
     <div className="min-h-screen bg-gray-100 py-10">
       <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold text-center mb-8">Posts</h1>
+        <Hello />
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
           {posts.map((post) => (
             <div
---
+++ components/Hello.tsx
@@ -0,0 +1,6 @@
+"use client";
+const Hello = () => {
+  return <button onClick={() => alert("Hello")}>Print Hello</button>;
+};
+
+export default Hello;
